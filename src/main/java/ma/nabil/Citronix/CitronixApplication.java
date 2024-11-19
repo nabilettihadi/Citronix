cc32548bc2691f731eda8e0f0a9f4259898f7fc3
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-11-19 11:02:08 +0100

    [CITX-1] feat: Initial project configuration

---
+++ src/main/java/ma/nabil/Citronix/CitronixApplication.java
@@ -0,0 +1,13 @@
+package ma.nabil.Citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitronixApplication.class, args);
+	}
+
+}
