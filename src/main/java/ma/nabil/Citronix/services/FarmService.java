5c478366102e56e8a21e6576fafc80754b417756
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-11-20 18:52:36 +0100

    CIT-9 Modification d'une Ferme

--- src/main/java/ma/nabil/Citronix/services/FarmService.java
+++ src/main/java/ma/nabil/Citronix/services/FarmService.java
@@ -6,4 +6,6 @@ import ma.nabil.Citronix.dtos.responses.FarmResponse;
 
 public interface FarmService {
     FarmResponse create(FarmRequest request);
+    FarmResponse update(Long id, FarmRequest request);
+
 }
