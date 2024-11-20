75eb142561a35f640422d756d6271ee3a376fbff
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-11-20 20:11:42 +0100

    CIT-25 Ajout de GetById et GetAll pours récupérer les fermes et les validations pour farm creation et update

--- src/main/java/ma/nabil/Citronix/services/FarmService.java
+++ src/main/java/ma/nabil/Citronix/services/FarmService.java
@@ -3,9 +3,15 @@ package ma.nabil.Citronix.services;
 import ma.nabil.Citronix.dtos.requests.FarmRequest;
 import ma.nabil.Citronix.dtos.responses.FarmResponse;
 
+import java.util.List;
+
 
 public interface FarmService {
     FarmResponse create(FarmRequest request);
+
     FarmResponse update(Long id, FarmRequest request);
 
+    FarmResponse getById(Long id);
+
+    List<FarmResponse> getAll();
 }
